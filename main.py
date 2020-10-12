8b89ec9775b8a01ce04fa5b06fafd12924aea18c
Peter Froud <pfroud@gmail.com>
2020-10-11 17:59:06 -0700

    made fade in/out wrap around

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from neopixel_ring_clock import NeopixelRingClock
 from window_blinds import WindowBlinds
 import traceback
 import dateparser
+import board
 
 
 DATETIME_FORMAT_STRING = "%A %d %B %Y, %I:%M:%S %p"
@@ -48,72 +49,17 @@ def run_clock():
     def clock_tick():
         text.value = datetime.datetime.now().strftime("%I:%M:%S %p")        
     app.repeat(1000, clock_tick)
+  
 
 
-
-
-def make_motor_gui():
-   
-    pwm_pin = 16 #GPIO 16 = rpi pin 36
-    direction_pin = 12 #GPIO 12 = rpi pin 32
-    
-    guizero_app = guizero.App(title="Motor tester")
-    guizero.Text(guizero_app, text="Set duty cycle:")
-    
-    
-    window_blinds = WindowBlinds(guizero_app, direction_pin, pwm_pin)
-    
-    def handle_slider_change(new_value_str):
-        window_blinds.pwm_device.value = int(new_value_str)/100
-    guizero.Slider(guizero_app, command=handle_slider_change, start=0, end=100, width="fill")
-
-    guizero_checkbox = None
-    
-    def handle_checkbox():
-        window_blinds.direction_device.value = guizero_checkbox.value
-    guizero_checkbox = guizero.CheckBox(guizero_app, text="Other direction", command=handle_checkbox)
-    
-    window_blinds.direction_device.value = guizero_checkbox.value
-    
-    
-    box_calibration_open = guizero.Box(guizero_app)
-    button_set_open = guizero.PushButton(box_calibration_open, text="Set open", align="left")
-    text_open = guizero.Text(box_calibration_open, text=window_blinds.accelerometer_Gs_open, align="left")
-    def set_cal_open():
-        text_open.value = "{:4.2f}".format(window_blinds.set_cal_open())
-    button_set_open.update_command(set_cal_open)
-
-
-    box_calibration_closed = guizero.Box(guizero_app)
-    button_set_closed = guizero.PushButton(box_calibration_closed, text="Set closed", align="left")
-    text_closed = guizero.Text(box_calibration_closed, text=window_blinds.accelerometer_Gs_closed, align="left")
-    def set_cal_closed():
-        text_closed.value = "{:4.2f}".format(window_blinds.set_cal_closed())
-    button_set_closed.update_command(set_cal_closed)
-        
-    
-    box_go_to = guizero.Box(guizero_app)
-    def go_to_closed():
-        window_blinds.go_to_closed()
-    def go_to_open():
-        window_blinds.go_to_open()
-    guizero.PushButton(box_go_to, text="Go to open", align="left", command=go_to_open)
-    guizero.PushButton(box_go_to, text="Go to closed", align="left", command=go_to_closed)
-
-    guizero_app.display() #blocks until the guizero window is closed
-    window_blinds.stop()
-    
-
-
-def make_clock_gui():
+def make_neopixel_clock_gui():
     
     guizero_app = guizero.App(title="Neopixel tester")
     
-    pin = 18 #GPIO 18 = rpi pin 12
+    pin = board.D18 #GPIO 18 = rpi pin 12
     led_count = 35
     brightness_max = 10
-    use_smoothing = True
-    clock = NeopixelRingClock(pin, led_count, brightness_max, use_smoothing)
+    clock = NeopixelRingClock(pin, led_count, brightness_max)
 
     box_hour = guizero.Box(guizero_app, width="fill")
     guizero.Text(box_hour, text="Hour: ", align="left")
@@ -125,7 +71,7 @@ def make_clock_gui():
     #open_blinds_led_index = round(led_count * open_blinds_hour / 12)
 
    
-    run_clock = True
+    
 
 
     def handle_time_slider():
@@ -134,7 +80,7 @@ def make_clock_gui():
             int(slider_minute.value),
             int(slider_second.value)
         )
-    slider_hour = guizero.Slider(box_hour, command=handle_time_slider, start=0, end=12, width="fill", align="left") 
+    slider_hour = guizero.Slider(box_hour, command=handle_time_slider, start=0, end=11, width="fill", align="left") 
      
 
     box_minute = guizero.Box(guizero_app, width="fill")
@@ -153,10 +99,10 @@ def make_clock_gui():
     slider_brightness = guizero.Slider(box_brightness, command=handle_brightness_slider, start=1, end=255, width="fill", align="left")
     slider_brightness.value = brightness_max
 
-    
-    interval_ms = 1000 * 10
-    
-    guizero_app.repeat(interval_ms, clock.tick)
+    run_clock = False
+    if run_clock:
+        interval_ms = 1000 * 10
+        guizero_app.repeat(interval_ms, clock.tick)
     clock.tick()
     
      
@@ -202,7 +148,7 @@ def make_end_user_gui():
         ######## when to close the blinds - get from sunrise API
         
         is_pm = now.hour >= 12
-        date_to_get_sunrise_for = None
+#        date_to_get_sunrise_for = None #apparently we don't need to forward-declare this
         
         if is_pm:
             # schedule the alarm for tomorrow
@@ -314,4 +260,4 @@ def make_end_user_gui():
      
 
 if __name__ == '__main__':
-    make_end_user_gui()
+    make_neopixel_clock_gui()
--- neopixel_ring_clock.py
+++ neopixel_ring_clock.py
@@ -3,10 +3,9 @@ from neopixel import NeoPixel
 from datetime import datetime
 
 class NeopixelRingClock:
-    def __init__(self, pin, led_count, max_brightness, use_smoothing):
+    def __init__(self, pin, led_count, max_brightness):
          self.neopixels = NeoPixel(pin, led_count)
          self.max_brightness = max_brightness
-         self.use_smoothing = use_smoothing
          self.led_count = led_count
          
     def set_max_brightness(self, new_max_brightness):
@@ -18,39 +17,60 @@ class NeopixelRingClock:
         minute_ratio = (minute + second_ratio) / 60
         hour_ratio = (hour + minute_ratio) / 12
         
-        hour_led_index = round(self.led_count * hour_ratio)
-        minute_led_index = round(self.led_count * minute_ratio)
-        
         for i in range(0, self.led_count):
             
-            if self.use_smoothing:
-                led_ratio = i / self.led_count
-                hour_distance = abs(led_ratio - hour_ratio)
-                minute_distance = abs(led_ratio - minute_ratio)
+            if i==0 and hour==11 and minute>30:
+                # The hour hand has almost gone back around to 12 o'clock.
+                # We want the LED with index zero to start fading in.
                 
-                rise = self.max_brightness
-                run = 1 / self.led_count
-                slope = -rise / run
-                
-                red = max(0, slope * hour_distance + self.max_brightness)
-                blue = max(0, slope * minute_distance + self.max_brightness)
-                
-                
-                
+                # The for loop will set i to 0, 1, ..., led_count-2, led_count-1.
+                # Do the computation for an imaginary extra LED but assign the
+                # result to index zero.
+                led_index_to_compute = self.led_count
+                led_index_to_set = 0
             else:
-                red = self.max_brightness if i==hour_led_index else 0
-                blue = self.max_brightness if i==minute_led_index else 0
-               
-            green = 0 #5 if (i >= close_blinds_led_index and i <= open_blinds_led_index) else 0
-          
+                led_index_to_compute = i
+                led_index_to_set = i
             
-            self.neopixels[i] = (red, green, blue)
+            
+            led_ratio = led_index_to_compute / self.led_count
+            hour_distance = abs(led_ratio - hour_ratio)
+            minute_distance = abs(led_ratio - minute_ratio)
+            
+            # Graph of LED duty cycle vs distance the LED is to the clock hand
+            # when led_count==35
+            #
+            #   255| \
+            # d    |  \
+            # u    |   \
+            # t    |    \
+            # y    |     \
+            #      |      \
+            # c    |       \
+            # y    |        \
+            # c    |         \
+            # l    |          \
+            # e    |           \
+            #     0+-----------------
+            #      0          (1/35)
+            #         distance
+            
+            rise = -self.max_brightness
+            run = 1 / self.led_count
+            slope = rise / run
+            
+            red = max(0, slope * hour_distance + self.max_brightness)
+            blue = max(0, slope * minute_distance + self.max_brightness)
+            
+            green = 0 #5 if (i >= close_blinds_led_index and i <= open_blinds_led_index) else 0
+            self.neopixels[led_index_to_set] = (red, green, blue)
             
     def tick(self):
             now = datetime.now()
             hour = now.hour
             if hour > 11:
-                # 12 PM becomes hour zero
+                # Change 24-hour time to 12-hour time.
+                # 12 P.M. becomes hour zero, 1 P.M. becomes hour one, etc.
                 hour = hour - 12
             minute = now.minute
             second = now.second
