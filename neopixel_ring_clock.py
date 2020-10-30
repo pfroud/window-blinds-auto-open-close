a293e5bb2f87318cfc39b368c6563d74f367908f
Peter Froud <pfroud@gmail.com>
2020-10-30 06:31:36 -0700

    added display of alarm region

--- neopixel_ring_clock.py
+++ neopixel_ring_clock.py
@@ -7,7 +7,12 @@ class NeopixelRingClock:
          self.neopixels = NeoPixel(pin, led_count)
          self.max_brightness = max_brightness
          self.led_count = led_count
-         self.alarm_regions = {}
+         self.alarm_regions = {
+            "test": [
+                datetime(2020, 10, 30, 9, 0, 0),
+                datetime(2020, 10, 30, 12+3, 0, 0)
+            ]
+         }
          
     def set_max_brightness(self, new_max_brightness):
          self.max_brightness = new_max_brightness
@@ -64,15 +69,9 @@ class NeopixelRingClock:
     
     def update(self):
         now = datetime.now()
+        now_is_pm = now.hour > 11
         
         hour_ratio, minute_ratio, second_ratio = self._get_ratios_for_datetime(now)
-        
-        #for array_of_datetimes in alarm_regions.values():
-        #   alarm_start_datetime = array_of_datetimes[0]
-        #    alarm_start_is_pm = alarm_start_datetime.hour > 11
-        #    if now_is_pm == alarm_start_is_pm:
-                
-                
         
         for i in range(0, self.led_count):
             
@@ -93,5 +92,30 @@ class NeopixelRingClock:
             red = self._get_duty_cycle_for_ratio(led_index_to_compute, hour_ratio)
             blue = self._get_duty_cycle_for_ratio(led_index_to_compute, minute_ratio)
             
-            green = 0 #5 if (i >= close_blinds_led_index and i <= open_blinds_led_index) else 0
+            
+            for array_of_datetimes in self.alarm_regions.values():
+                region_start_datetime = array_of_datetimes[0]
+                region_end_datetime = array_of_datetimes[1]
+                
+                #if region_end_datetime > region_start_datetime:
+                
+                region_start_hour_ratio, not_used, not_used = self._get_ratios_for_datetime(region_start_datetime)
+                region_end_hour_ratio, not_used, not_used = self._get_ratios_for_datetime(region_end_datetime)
+                led_ratio = led_index_to_compute / self.led_count
+                
+                alarm_start_is_pm = region_start_datetime.hour > 11
+                if now_is_pm == alarm_start_is_pm:
+                    
+                    if led_ratio < region_start_hour_ratio:
+                        green = self._get_duty_cycle_for_ratio(led_index_to_compute, region_start_hour_ratio)
+                    elif region_start_hour_ratio <= led_ratio <= region_end_hour_ratio:
+                        green = self.max_brightness
+                    elif led_ratio > region_end_hour_ratio:
+                         green = self._get_duty_cycle_for_ratio(led_index_to_compute, region_end_hour_ratio)
+                    else:
+                        green = 0
+                    
+                else:
+                    green = 0
+            
             self.neopixels[led_index_to_set] = (red, green, blue)
