9ea70daa0c7dac3b3919ac59a00a037d124b6447
Peter Froud <pfroud@gmail.com>
2022-06-26 14:18:30 -0700

    Run autopep8 formatter

--- hardware_test_scripts/neopixel_test.py
+++ hardware_test_scripts/neopixel_test.py
@@ -4,24 +4,24 @@ import os
 import time
 import board
 import neopixel
- 
+
 
 # Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
 # NeoPixels must be connected to D10, D12, D18 or D21 to work.
 pixel_pin = board.D21
- 
+
 # The number of NeoPixels
 num_pixels = 35
- 
+
 # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
 # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
 ORDER = neopixel.GRB
- 
+
 pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
 )
- 
- 
+
+
 def wheel(pos):
     # Input a value 0 to 255 to get a color value.
     # The colours are a transition r - g - b - back to r.
@@ -42,8 +42,8 @@ def wheel(pos):
         g = int(pos * 3)
         b = int(255 - pos * 3)
     return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
- 
- 
+
+
 def rainbow_cycle(wait):
     for j in range(255):
         for i in range(num_pixels):
@@ -51,7 +51,8 @@ def rainbow_cycle(wait):
             pixels[i] = wheel(pixel_index & 255)
         pixels.show()
         time.sleep(wait)
- 
+
+
 def main():
     print("Press Ctrl+C to stop")
     while True:
@@ -62,12 +63,13 @@ def main():
         pixels.fill((0, 100, 0))
         pixels.show()
         time.sleep(1.0)
-     
+
         pixels.fill((0, 0, 100))
         pixels.show()
         time.sleep(1.0)
-     
+
         rainbow_cycle(0.01)  # rainbow cycle with 1ms delay per step
+
 
 if __name__ == '__main__':
     if os.geteuid() == 0:
--- hardware_test_scripts/turn_on_neopixels.py
+++ hardware_test_scripts/turn_on_neopixels.py
@@ -3,11 +3,13 @@ import os
 import board
 import neopixel
 
+
 def main():
     pixels = neopixel.NeoPixel(
         board.D21, 35, brightness=1.0, auto_write=True, pixel_order=neopixel.GRB
     )
-    pixels.fill((1, 1, 1)) # RGB out of 255
+    pixels.fill((1, 1, 1))  # RGB out of 255
+
 
 if __name__ == '__main__':
     if os.geteuid() == 0:
